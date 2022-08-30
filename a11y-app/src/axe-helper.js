dbc9fd4a813fae86b6a475df4f45fdd720efe8b5
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-08-30 18:36:01 +0200

    feature: disable errors

--- a11y-app/src/axe-helper.js
+++ a11y-app/src/axe-helper.js
@@ -2,8 +2,12 @@ const { configureAxe } = require('jest-axe')
 
 const axe = configureAxe({
   rules: {
-
-  }, 
+    // for demonstration only, don't disable rules that need fixing.
+    'image-alt': { enabled: false },
+    "aria-required-children": { "enabled": false },
+    "color-contrast": { "enabled": false },
+    "list": { "enabled" : false}
+  },
   impactLevels: ['critical','serious','moderate','minor']
 })
 
